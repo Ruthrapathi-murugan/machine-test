1a53c761a0ee11c43daf54527bd452dd8eafae6f
Ruthrapathi <ruthrapathimurugan@outlook.com>
2024-09-25 21:21:09 +0530

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="home-container">
       <h1>Welcome to the Home Page</h1>
-      <p>This is a simple, clean homepage designed for a machine test.</p>
+      <p>This is a simple, clean homepage  designed for a machine test.</p>
       <button className="home-button">Explore More</button>
     </div>
   );
